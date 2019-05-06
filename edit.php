a9bde907b517a239df60e45a1c6fd9567c251395
smnshuvo <smnshuvo@gmail.com>
2019-05-06 09:12:33 +0600

    Added Meal Editing

---
+++ edit.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>
+            Add your meal
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/form.css">
+    </head>
+    
+    <body>
+        <h1> Select the meal you want to edit </h1> 
+        <form method="post" action="editMeal.php">
+            <br> Date: <input type="text" name="Date"> 
+         <br> Shuvo: <input type="text" name="Shuvo"> 
+            Touhid: <input type="text" name="Touhid"> 
+          Mahir: <input type="text" name="Mahir">  
+           <br> Mehedi <input type="text" name="Mehedi"> 
+           Mahmud <input type="text" name="Mahmud"> 
+            Anik <input type="text" name="Anik"> 
+           <br> Total <input type="text" name="Amount">
+         <br> Done by: <input type="text" name="done_by">
+          <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+
+
---
+++ editMeal.php
@@ -0,0 +1,39 @@
+<?php
+/* 
+ *  This project is developed my SMN Shuvo
+ *  Copyright Protected, Project must be used with proper credit  * 
+ */
+
+include ('connect.php');
+$date = filter_input(INPUT_POST, 'Date');
+$shuvo = filter_input(INPUT_POST, 'Shuvo');
+$touhid = filter_input(INPUT_POST, 'Touhid');
+$mahir = filter_input(INPUT_POST, 'Mahir');
+$mehedi = filter_input(INPUT_POST, 'Mehedi');
+$mahmud = filter_input(INPUT_POST, 'Mahmud');
+$anik = filter_input(INPUT_POST, 'Anik');
+$amount = filter_input(INPUT_POST, 'Amount');
+$done_by = filter_input(INPUT_POST, 'done_by');
+// form input section 
+
+$sql = "UPDATE `mess_manager` SET "
+        . "`Shuvo` = $shuvo, "
+        . "`Touhid` = $touhid, "
+        . "`Mahir` = $mahir, "
+        . "`Mehedi` = $mehedi, "
+        . "`Mahmud` = '34', "
+        . "`Anik` = '33', "
+        . "`Amount` = '4304', "
+        . "`done_by` = '343' "
+        . "WHERE `mess_manager`.`Date` = $date";
+
+if ($cnct->query($sql)){
+    echo "Data edited";
+    echo "<br> <a href='meal.php'> Click Here to See the meals </a>";
+}
+else {
+    echo "error" . $cnct->error;
+}
+
+ 
+ ?>
